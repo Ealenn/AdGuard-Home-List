260374c91dc65d83b99e726a61b6abee1bbe8002
Rudy Marchandise <r.marchandise@sfr.fr>
2022-07-23 15:11:09 +0200

    fix: console logger

--- modules/guard-cli/src/services/log.service.ts
+++ modules/guard-cli/src/services/log.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, ConsoleLogger } from '@nestjs/common';
 
 @Injectable()
-export class LogService extends Logger {}
+export class LogService extends ConsoleLogger {}
